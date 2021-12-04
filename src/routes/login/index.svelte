0d31744af9d0e747010e3f3a21f6db4775b16fd4
FatBanana <yifanw06@gmail.com>
2021-12-03 18:58:20 -0800

    add login redirect

--- src/routes/login/index.svelte
+++ src/routes/login/index.svelte
@@ -1,3 +1,15 @@
+<script context="module">
+	export async function load({ session }) {
+		if (session.user) {
+			return {
+				status: 302,
+				redirect: '/login'
+			}
+		}
+		return {}
+	}
+</script>
+
 <script>
 	import { session } from '$app/stores'
 	import { goto } from '$app/navigation'
